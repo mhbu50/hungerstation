6972cf7b0316fc96f250303510d12cee5d92880b
mhbu50 <mhbu50@gmail.com>
2017-08-30 03:01:02 +0300

    add code to area

--- hungerstation/hungerstation/doctype/area/area.js
+++ hungerstation/hungerstation/doctype/area/area.js
@@ -2,7 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Area', {
-	refresh: function(frm) {
-
+	validate: function(frm) {
+		frm.set_value("area",frm.doc.area.charAt(0).toUpperCase() + frm.doc.area.slice(1));
+		frm.set_value("area_code",frm.doc.area_code.toUpperCase());
 	}
 });
